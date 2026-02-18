f523625af70d1c0f9dd67336ecca13573eb9098c
leodamuge <damugeleonardo@gmail.com>
2026-02-18 23:56:52 +0200

    c 18-2

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,23 @@ function App() {
 
 useEffect(() => {
   localStorage.setItem("tasks", JSON.stringify(tasks))
-}, [tasks])
+}, [tasks]);
+
+useEffect(() => {
+  async function fetchTasks(){
+  
+  const response = await fetch(
+    'https://jsonplaceholder.typicode.com/todos?_limit=10',
+  {
+    method: "GET",
+  });
+  
+  const data = await response.json();
+  setTasks(data);
+  };
+  //Chamador da API
+  //fetchTasks();
+}, []);
 
 function onTaskClick(taskId){
   const newTasks = tasks.map (task => {
